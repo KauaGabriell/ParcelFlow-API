0857ecc9fa2028333a9fed8b8cc03d3b0aea2e14
Kaua Gabriell <kauagabriell64@gmail.com>
2026-04-25 01:19:49 -0300

    fix(auth): correct password comparison flow and session credential validation

--- src/controllers/sessionsControllers.ts
+++ src/controllers/sessionsControllers.ts
@@ -1,8 +1,24 @@
 import { Request, Response } from 'express';
+import { prisma } from '../database/prisma';
+import { z } from 'zod';
+import { comparePassword } from '@/utils/hashPassword';
+import { AppError } from '@/utils/AppError';
 
 class SessionController {
-  create(request: Request, response: Response) {
-    response.json({ message: 'ok' });
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      email: z.email(),
+      password: z.string().min(6),
+    });
+
+    const { email, password } = bodySchema.parse(request.body);
+    const user = await prisma.user.findFirst({ where: { email } });
+
+    if (!user) throw new AppError('Invalid Credentials', 401);
+
+    await comparePassword(password, user.password);
+
+    return response.json({ message: 'ok' });
   }
 }
 
--- src/utils/hashPassword.ts
+++ src/utils/hashPassword.ts
@@ -1,8 +1,19 @@
 import bcrypt from 'bcrypt';
+import { AppError } from './AppError';
 
 export async function hashPassword(password: string) {
   const rounds = 12;
 
   const hash = await bcrypt.hash(password, rounds);
   return hash;
+}
+
+export async function comparePassword(password: string, hash?: string) {
+  if (!hash) throw new AppError('Invalid Credentials', 401);
+
+  const comparedPassword = await bcrypt.compare(password, hash);
+
+  if (!comparedPassword) throw new AppError('Invalid Credentials', 401);
+
+  return comparedPassword;
 }
