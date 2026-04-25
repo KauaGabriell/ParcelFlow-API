85e83cbe8d68239e9eb06a0024a9111f6dcb2f03
Kaua Gabriell <kauagabriell64@gmail.com>
2026-04-25 15:53:59 -0300

    feat(deliveries): add ensure authentication in deliveries routes

--- src/routes/deliveriesRoutes.ts
+++ src/routes/deliveriesRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
 import { DeliveriesController } from '@/controllers/deliveriesControllers';
+import { ensureAuthentication } from '@/middlewares/ensureAuthentication';
 
 const deliveriesRoutes = Router();
 const deliveriesController = new DeliveriesController();
 
+deliveriesRoutes.use(ensureAuthentication);
 deliveriesRoutes.post('/', deliveriesController.create);
 
 export { deliveriesRoutes };
