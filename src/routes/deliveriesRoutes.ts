efa7a9ce46f43a3ee2176e2bd2a82e3bb0b95d2d
Kaua Gabriell <kauagabriell64@gmail.com>
2026-04-25 16:20:03 -0300

    feat(deliveries): add authorization in deliveries routes.

--- src/routes/deliveriesRoutes.ts
+++ src/routes/deliveriesRoutes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 
 import { DeliveriesController } from '@/controllers/deliveriesControllers';
 import { ensureAuthentication } from '@/middlewares/ensureAuthentication';
+import { verifyAuthorizationUser } from '@/middlewares/verifyAuthorizationUser';
 
 const deliveriesRoutes = Router();
 const deliveriesController = new DeliveriesController();
 
 deliveriesRoutes.use(ensureAuthentication);
+deliveriesRoutes.use(verifyAuthorizationUser(['sale']));
 deliveriesRoutes.post('/', deliveriesController.create);
 
 export { deliveriesRoutes };
